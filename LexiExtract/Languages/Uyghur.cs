ac7d80d9569903f170e9d40ecd567c87e1fe44c8
eymenefealtun <eymenefealtun@hotmail.com>
2023-11-26 14:57:24 +0300

    feat: uyghur added

---
+++ LexiExtract/Languages/Uyghur.cs
@@ -0,0 +1,15 @@
+﻿namespace LexiExtract.Languages
+{
+    public class Uyghur : Language
+    {
+        public override string[] GetAllWords()
+        {
+            return Helpers.ExtractArray(Helpers.GetMainUrl(this.GetType().Name));
+        }
+
+        public override Task<string[]> GetAllWordsAsync()
+        {
+            return Helpers.ExtractArrayAsync(Helpers.GetMainUrl(this.GetType().Name));
+        }
+    }
+}
