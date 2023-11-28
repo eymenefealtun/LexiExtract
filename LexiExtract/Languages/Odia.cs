4b8b9211660d2e642a2b4942910f68028689a98a
eymenefealtun <eymenefealtun@hotmail.com>
2023-11-28 14:19:41 +0300

    feat: odia added

---
+++ LexiExtract/Languages/Odia.cs
@@ -0,0 +1,15 @@
+﻿namespace LexiExtract.Languages
+{
+    public class Odia : Language
+    {
+        public override string[] GetAllWords()
+        {
+            return Helpers.ExtractArray(Helpers.GetMainUrl(this.GetType().Name));
+        }
+
+        public override Task<string[]> GetAllWordsAsync()
+        {
+            return Helpers.ExtractArrayAsync(Helpers.GetMainUrl(this.GetType().Name));
+        }
+    }
+}
