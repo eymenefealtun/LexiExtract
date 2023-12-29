427dc97d0ac2f19c2397386703ee02f4c52e54e0
eymenefealtun <eymenefealtun@hotmail.com>
2023-12-29 23:43:53 +0300

    feat: scots gaelic added

---
+++ LexiExtract/Languages/ScotsGaelic.cs
@@ -0,0 +1,16 @@
+﻿namespace LexiExtract.Languages
+{
+    public class ScotsGaelic : Language
+    {
+
+        public override string[] GetAllWords()
+        {
+            return Helpers.ExtractArray(Helpers.GetMainUrl("Scots%20Gaelic"));
+        }
+        public override Task<string[]> GetAllWordsAsync()
+        {
+            return Helpers.ExtractArrayAsync(Helpers.GetMainUrl("Scots%20Gaelic"));
+        }
+    }
+
+}
