b3d4ec5e5f78e80e261a9ea3f8aa0e85d56031d6
eymenefealtun <eymenefealtun@hotmail.com>
2024-01-10 00:33:07 +0300

    feat: haitian creole

---
+++ LexiExtract/Languages/HaitianCreole.cs
@@ -0,0 +1,16 @@
+﻿namespace LexiExtract.Languages
+{
+    public class HaitianCreole : Language
+    {
+
+        public override string[] GetAllWords()
+        {
+            return Helpers.ExtractArray(Helpers.GetMainUrl("Haitian%20Creole"));
+        }
+        public override Task<string[]> GetAllWordsAsync()
+        {
+            return Helpers.ExtractArrayAsync(Helpers.GetMainUrl("Haitian%20Creole"));
+        }
+    }
+
+}
